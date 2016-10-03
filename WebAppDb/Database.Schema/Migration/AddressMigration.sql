baff949eccccedc9b86c1aa6520d32d091d642a7
dmisav <savchenko.di@gmail.com>
2016-10-03 22:57:30 +0300

    Added base projects

---
+++ WebAppDb/Database.Schema/Migration/AddressMigration.sql
@@ -0,0 +1,22 @@
+﻿MERGE INTO Address AS Target 
+USING (VALUES 
+  (0, N'Undefined'), 
+  (1, N'Billing'), 
+  (2, N'Home'), 
+  (3, N'Main Office'), 
+  (4, N'Primary'), 
+  (5, N'Shipping'), 
+  (6, N'Archive') 
+) 
+AS Source (ID, Name) 
+ON Target.ID = Source.ID 
+
+WHEN MATCHED THEN 
+UPDATE SET Name = Source.Name 
+
+WHEN NOT MATCHED BY TARGET THEN 
+INSERT (ID, Name) 
+VALUES (ID, Name) 
+
+WHEN NOT MATCHED BY SOURCE THEN 
+DELETE;
---
+++ WebAppDb/Database.Schema/dbo/Address.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE [dbo].[Address]
+(
+	[Id] INT NOT NULL PRIMARY KEY,
+	[Name] VARCHAR(500) NOT NULL
+)
